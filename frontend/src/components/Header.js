890e0d91ee4754cab62d60501fa0a0f9fab66e33
alexnesov <alexandrenesovic@gmail.com>
2023-10-03 15:01:05 +0200

    reactified href's in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,29 @@
 import React from 'react'
-import { Navbar, Nav, Container, Row } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
   return (
         <header>
                 <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-                <Container>
-                    <Navbar.Brand href="/">LPC </Navbar.Brand>
+                 <Container>
+
+                    <LinkContainer to='/'>
+                      <Navbar.Brand>LPC</Navbar.Brand>
+                    </LinkContainer>
+
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/panier"><i className='fas fa-shopping-cart'></i>Panier</Nav.Link>
-                        <Nav.Link href="/connexion"><i className='fas fa-user'></i>Connexion</Nav.Link>
+
+                    <LinkContainer to='/cart'>
+                        <Nav.Link><i className='fas fa-shopping-cart'></i>Panier</Nav.Link>
+                    </LinkContainer>
+
+                    <LinkContainer to='/connexion'>
+                        <Nav.Link><i className='fas fa-user'></i>Connexion</Nav.Link>
+                    </LinkContainer>
+
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
