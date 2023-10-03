48bbabfc44e97ca2a0b867b1af5f1525bcfe4f92
alexnesov <alexandrenesovic@gmail.com>
2023-10-03 12:52:17 +0200

    routing init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,29 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
 
       <main className='py-3'>
-      <Container>
-        <h1>LPC Ecommerce</h1>
-      </Container>
+        <Container>
+          <h1>LPC E-commerce</h1>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
       </main>
+
       <Footer />
-    </div>
+    </Router>
   );
 }
 
