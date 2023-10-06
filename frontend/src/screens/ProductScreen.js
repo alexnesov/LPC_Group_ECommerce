418c019f2883af533a0ba3de9b5c52a684516abb
alexnesov <alexandrenesovic@gmail.com>
2023-10-06 20:49:31 +0200

    link to product impl

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card} from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
+import { useParams } from 'react-router-dom';
 
-function ProductScreen() {
+function ProductScreen( {match} ) {
+
+
+  const { id } = useParams();
+
+  const product = products.find((p) => p._id == id);
+
   return (
     <div>
-        Product
+        <Link to='/' className='btn my-3'>Go Back</Link>
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} / >
+          </Col>
+        </Row>
     </div>
   )
 }
